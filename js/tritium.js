b763f8cdc270305741b293f56b31bb7b9334cd53
syb0rg <darksyb0rg@gmail.com>
2014-07-22 15:19:57 -0600

    Modified color of atom.svg, added some code for animating SVG.

--- js/tritium.js
+++ js/tritium.js
@@ -5,6 +5,10 @@ $(window).scroll(function () {
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
+    var s = ($(window).scrollTop() / ($(document).height() - $(window).height()));
+    var r1 = 40+106*s, r2=160-140*s;
+    $("#a1").attr("transform","rotate("+r1+")");
+    $("#a2").attr("transform","rotate("+r2+")");
 });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
@@ -13,7 +17,7 @@ $(function () {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        }, 1000, 'easeInOutExpo');
         event.preventDefault();
     });
 });
