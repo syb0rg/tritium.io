02b87bd4330ba4cd078d7e764af22a164fcaed18
syb0rg <darksyb0rg@gmail.com>
2014-08-03 13:26:43 -0500

    Fixed rendering of atom on mobile devices, started adding a popup.

--- js/tritium.js
+++ js/tritium.js
@@ -1,17 +1,18 @@
-//jQuery to collapse the navbar on scroll
 $(window).scroll(function () {
+    // jQuery to collapse the navbar on scroll
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
+    // jQuery to animate atom SVG
     var s = ($(window).scrollTop() / (($("#intro").height() + $("#about").height()) - $(window).height()));
     var r1 = 40+106*s, r2=160-100*s;
     $("#a1").attr("transform","rotate("+r1+")");
     $("#a2").attr("transform","rotate("+r2+")");
 });
 
-//jQuery for page scrolling feature - requires jQuery Easing plugin
+// jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function () {
     $('.page-scroll a').bind('click', function (event) {
         var $anchor = $(this);
@@ -20,4 +21,39 @@ $(function () {
         }, 1000, 'easeInOutExpo');
         event.preventDefault();
     });
+});
+
+$(document).ready(function() {
+$('a.download-window').click(function() {
+    $("body").css("overflow", "hidden");
+            //Getting the variable's value from a link 
+    var loginBox = $(this).attr('href');
+
+    //Fade in the Popup
+    $(loginBox).fadeIn(500);
+    
+    //Set the center alignment padding + border see css style
+    var popMargTop = ($(loginBox).height() + 24) / 2; 
+    var popMargLeft = ($(loginBox).width() + 24) / 2; 
+    
+    $(loginBox).css({ 
+        'margin-top' : -popMargTop,
+        'margin-left' : -popMargLeft
+    });
+    
+    // Add the mask to body
+    $('body').append('<div id="mask"></div>');
+    $('#mask').fadeIn(300);
+    
+    return false;
+});
+
+// When clicking on the button close or the mask layer the popup closed
+$('a.close, #mask').on('click', function() { 
+  $('#mask , .download-popup').fadeOut(300 , function() {
+    $('#mask').remove();  
+    $("body").css("overflow", "auto");
+}); 
+return false;
+});
 });
