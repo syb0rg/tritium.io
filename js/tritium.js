15671b3f4bed4c2b5649fd36faa94fe1656eac04
syb0rg <darksyb0rg@gmail.com>
2014-08-15 21:01:16 -0500

    Made popup actually relevent to my website.

--- js/tritium.js
+++ js/tritium.js
@@ -32,15 +32,6 @@ $('a.download-window').click(function() {
     //Fade in the Popup
     $(loginBox).fadeIn(500);
     
-    //Set the center alignment padding + border see css style
-    var popMargTop = ($(loginBox).height() + 24) / 2; 
-    var popMargLeft = ($(loginBox).width() + 24) / 2; 
-    
-    $(loginBox).css({ 
-        'margin-top' : -popMargTop,
-        'margin-left' : -popMargLeft
-    });
-    
     // Add the mask to body
     $('body').append('<div id="mask"></div>');
     $('#mask').fadeIn(300);
